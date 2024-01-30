680f430377391ba981532be0c60921516b904f4a
Saints at Play <support@masteringionic.com>
2024-01-30 15:44:08 +0000

    Signals by Observable subscription and async pipe rendering within Ionic/Angular

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,83 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, 
+         signal, 
+         WritableSignal, 
+         AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IonHeader, 
+         IonToolbar, 
+         IonTitle, 
+         IonContent,
+         IonItem,
+         IonLabel,
+         IonAccordion,
+         IonAccordionGroup,
+     } from '@ionic/angular/standalone';
+import { Observable, map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, 
+            IonToolbar, 
+            IonTitle, 
+            IonContent, 
+            IonItem,
+            IonLabel,
+            IonAccordion,
+            IonAccordionGroup,
+            AsyncPipe],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements AfterViewInit {
+
+  
+  private remoteUri = 'https://airportsusingbodyscanners.com/sample-test.json';
+
+
+  public airports: WritableSignal<any> = signal<any | null>(null);
+  
+
+  public counter$!: Observable<Array<any>>;
+
+
+  public counter: WritableSignal<any> = signal<any | null>(null);
+
+
+  constructor(private http: HttpClient) {}
+
+
+  ngAfterViewInit(): void {
+    this.getBySubscribe();    
+    this.getByAsync();
+  }
+
+
+  public get(): Observable<any> {
+    return this.http.get<any>(this.remoteUri);
+  }
+
+
+  public getBySubscribe() {
+    this.get()
+    .subscribe((data) => {
+      this.airports.set(data['global-locations']);
+      console.log('airports', this.airports());
+    });
+  }
+
+
+  public getByAsync() {
+      this.counter$ = this.get()
+                          .pipe(
+                            map((data: any) => {
+                              this.counter.set(data['global-locations']);
+                              console.log('counter', this.counter());
+                              return data;
+                            })
+                          );      
+  }
+
+
 }
